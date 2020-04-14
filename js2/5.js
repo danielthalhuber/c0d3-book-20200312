a08a1df74b313a8affafbcf3ea92a48b040e04c8
Daniel Thalhuber <dev@uhf.li>
2020-04-14 07:06:44 -0700

    js02c05: solution: add

--- js2/5.js
+++ js2/5.js
@@ -2,13 +2,18 @@
  * Write a function called solution that
  *   Takes in an array of functions and a number,
  *   and calls every function input milliseconds later
- * @param {array} arr
- * @param {number} time
+ * @param {function[]} arr - functions to be called after the given delay
+ * @param {number} time - milliseconds to delay function calls
+ * @param {number} i - 'private', indicates current index of arr
  */
 
-const solution = (arr, time) => {
-}
+const solution = (arr = [], time = 0, i = 0) => {
+  if (i >= arr.length) return;
+
+  setTimeout(arr[i], time);
+  return solution(arr, time, i + 1);
+};
 
 module.exports = {
-  solution
-}
+  solution,
+};
