8a4a06638800f629ff15381f4ba5413cf6062b2d
Daniel Thalhuber <dev@uhf.li>
2020-04-14 07:23:44 -0700

    js02c08: solution: add

--- js2/8.js
+++ js2/8.js
@@ -5,11 +5,21 @@
  */
 
 const solution = () => {
-  Array.prototype.cMap = function (cb) {
-    return 0
-  }
-}
+  /**
+   * Call the given function once for each element in the array, returning an array with the results of each function call
+   * @param {function} cb - the function to call for each element
+   * @param {object} thisArg - value of this to be used by cb
+   * @param {number} i - 'private', corresponds to current index of array
+   * @param {*[]} res - 'private', contains cb return values
+   */
+  Array.prototype.cMap = function (cb, thisArg = this, i = 0, res = []) {
+    if (!cb || i >= this.length) return res;
+
+    res.push(cb.call(thisArg, this[i], i, this));
+    return this.cMap(cb, thisArg, i + 1, res);
+  };
+};
 
 module.exports = {
-  solution
-}
+  solution,
+};
