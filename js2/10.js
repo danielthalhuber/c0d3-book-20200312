2f40685396b2015aac3ab4618cb4e1ceaa3ef793
Daniel Thalhuber <dev@uhf.li>
2020-04-30 19:20:26 -0700

    js02c10: add solution, fix test

--- js2/10.js
+++ js2/10.js
@@ -5,11 +5,27 @@
  */
 
 const solution = () => {
-  Array.prototype.cFilter = function () {
-    return 0
-  }
-}
+  /**
+   * Call the given function once for each element in the array, if the function returns true, included the element in the returned array
+   * @param {function} cb - the function to call for each element
+   * @param {object} thisArg - value of this to be used by cb
+   * @param {number} i - 'private', corresponds to current index of array
+   * @param {*[]} res - 'private', array of filtered results
+   * @returns {*[]}
+   */
+  Array.prototype.cFilter = function (cb, thisArg = this, i = 0, res = []) {
+    if (!cb) {
+      throw new TypeError(
+        'missing argument 0 when calling function Array.prototype.cFilter'
+      );
+    }
+    if (i >= this.length) return res;
+
+    if (cb.call(thisArg, this[i], i, this)) res.push(this[i]);
+    return this.cFilter(cb, thisArg, i + 1, res);
+  };
+};
 
 module.exports = {
-  solution
-}
+  solution,
+};
